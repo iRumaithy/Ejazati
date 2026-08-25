c738a2effbd967fc179a6d25da1a89770386e1c8
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-25 18:29:43 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+class AuthHotfixInjector {
+  element(element) {
+    element.append(
+      '<script src="/auth-hotfix.js?v=1.1.1"></script>',
+      { html: true }
+    );
+  }
+}
 
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+    const contentType = response.headers.get("content-type") || "";
+
+    if (!contentType.includes("text/html")) {
+      return response;
+    }
+
+    return new HTMLRewriter()
+      .on("body", new AuthHotfixInjector())
+      .transform(response);
+  }
+};
