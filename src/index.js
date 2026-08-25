4c5b394ac23b63fa8315741fcd16532a1af2f654
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-25 20:56:18 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 class EjazatiScriptsInjector {
   element(element) {
     element.append(
-      '<script src="/auth-hotfix.js?v=1.2.2"></script>' +
-      '<script src="/ui-v1.2.0.js?v=1.2.2"></script>' +
-      '<script src="/ui-v1.2.1.js?v=1.2.2"></script>' +
-      '<script src="/ui-v1.2.2.js?v=1.2.2"></script>',
+      '<script src="/auth-hotfix.js?v=1.2.3"></script>' +
+      '<script src="/ui-v1.2.0.js?v=1.2.3"></script>' +
+      '<script src="/ui-v1.2.1.js?v=1.2.3"></script>' +
+      '<script src="/ui-v1.2.2.js?v=1.2.3"></script>' +
+      '<script src="/ui-v1.2.3.js?v=1.2.3"></script>',
       { html: true }
     );
   }
