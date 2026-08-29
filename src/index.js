77cd155e1b70d154ded4eaba114b4f0ac3f0421e
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-30 00:19:34 +0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 class EjazatiScriptsInjector {
   element(element) {
     element.append(
-      '<script src="/auth-hotfix.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.0.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.1.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.2.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.3.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.4.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.5.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.8.js?v=1.2.9"></script>' +
-      '<script src="/ui-v1.2.9.js?v=1.2.9"></script>',
+      '<script src="/auth-hotfix.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.0.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.1.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.2.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.3.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.4.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.5.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.9.js?v=1.2.10"></script>' +
+      '<script src="/ui-v1.2.10.js?v=1.2.10"></script>',
       { html: true }
     );
   }
@@ -17,8 +17,7 @@ class EjazatiScriptsInjector {
 export default {
   async fetch(request, env) {
     const response = await env.ASSETS.fetch(request);
-    const contentType = response.headers.get("content-type") || "";
-    if (!contentType.includes("text/html")) return response;
+    if (!(response.headers.get("content-type") || "").includes("text/html")) return response;
     return new HTMLRewriter().on("body", new EjazatiScriptsInjector()).transform(response);
   }
 };
